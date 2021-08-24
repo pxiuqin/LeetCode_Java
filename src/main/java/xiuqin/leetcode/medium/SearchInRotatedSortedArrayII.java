3e5d8f0e4f6502267916107b6880bd47763e6bb2
liangxiuqin <liangxiuqin@mininglamp.com>
2021-08-24 11:30:28 +0800

    add algo:Search In Rotated Sorted Array II

---
+++ src/main/java/xiuqin/leetcode/medium/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,94 @@
+package xiuqin.leetcode.medium;
+
+/**
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+ * 81.旋转有序数组中搜索之二
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ *
+ * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return true, otherwise return false.
+ *
+ * Example 1:
+ * Input: nums = [2,5,6,0,0,1,2], target = 0
+ * Output: true
+ *
+ * Example 2:
+ * Input: nums = [2,5,6,0,0,1,2], target = 3
+ * Output: false
+ *
+ * 这道是之前那道 Search in Rotated Sorted Array 的延伸，现在数组中允许出现重复数字，
+ * 这个也会影响我们选择哪半边继续搜索，由于之前那道题不存在相同值，
+ * 我们在比较中间值和最右值时就完全符合之前所说的规律：如果中间的数小于最右边的数，则右半段是有序的，若中间数大于最右边数，则左半段是有序的。
+ * 而如果可以有重复值，就会出现来面两种情况，[3 1 1] 和 [1 1 3 1]，对于这两种情况中间值等于最右值时，目标值3既可以在左边又可以在右边，那怎么办么，
+ * 对于这种情况其实处理非常简单，只要把最右值向左一位即可继续循环，如果还相同则继续移，直到移到不同值为止，然后其他部分还采用 Search in Rotated Sorted Array 中的方法
+ */
+public class SearchInRotatedSortedArrayII {
+  public boolean search(int A[], int key) {
+    int n = A.length;
+    if (n <= 0) {
+      return false;
+    }
+
+    if (n == 1) {
+      return A[0] == key ? true : false;
+    }
+
+    int low = 0, high = n - 1;
+    while (low <= high) {
+      if (A[low] < A[high] && (key < A[low] || key > A[high])) {
+        return false;
+      }
+
+      //if dupilicates, remove the duplication
+      while (low < high && A[low] == A[high]) {
+        low++;
+      }
+
+      int mid = low + (high - low) / 2;
+      if (A[mid] == key) {
+        return true;
+      }
+
+      //the target in non-rotated array
+      if (A[low] < A[mid] && key >= A[low] && key < A[mid]) {
+        high = mid - 1;
+        continue;
+      }
+
+      //the target in non-rotated array
+      if (A[mid] < A[high] && key > A[mid] && key <= A[high]) {
+        low = mid + 1;
+        continue;
+      }
+
+      //the target in rotated array
+      if (A[low] > A[mid]) {
+        high = mid - 1;
+        continue;
+      }
+
+      //the target in rotated array
+      if (A[mid] > A[high]) {
+        low = mid + 1;
+        continue;
+      }
+
+      //reach here means nothing found.
+      low++;
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    SearchInRotatedSortedArrayII obj = new SearchInRotatedSortedArrayII();
+
+    System.out.println(obj.search(new int[]{3, 3, 3, 4, 5, 6, 2, 3}, 2));
+    System.out.println(obj.search(new int[]{3, 3, 3, 4, 5, 6, 3, 3}, 4));
+    System.out.println(obj.search(new int[]{3, 3, 3, 3, 1, 3}, 1));
+    System.out.println(obj.search(new int[]{3, 3, 3, 2, 2, 1, 3, 3}, 1));
+    System.out.println(obj.search(new int[]{3, 3, 3, 2, 2, 1, 4, 3}, 4));
+  }
+}
