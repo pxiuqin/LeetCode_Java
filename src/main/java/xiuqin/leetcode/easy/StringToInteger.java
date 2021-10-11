ab7cab502052c925d0d603afc76c8d7dd4acca89
liangxiuqin <liangxiuqin@mininglamp.com>
2021-10-11 11:25:10 +0800

    update algo:String To Integer Atoi

--- src/main/java/xiuqin/leetcode/easy/StringToInteger.java
+++ src/main/java/xiuqin/leetcode/easy/StringToInteger.java
@@ -3,80 +3,83 @@ package xiuqin.leetcode.easy;
 
 /**
  * https://oj.leetcode.com/problems/string-to-integer-atoi/
- *
+ * <p>
  * 8.Implement atoi to convert a string to an integer.
- *
+ * <p>
  * Hint: Carefully consider all possible input cases. If you want a challenge,
- *       please do not see below and ask yourself what are the possible input cases.
- *
+ * please do not see below and ask yourself what are the possible input cases.
+ * <p>
  * Notes:
- *   It is intended for this problem to be specified vaguely (ie, no given input specs).
- *   You are responsible to gather all the input requirements up front.
- *
- *
+ * It is intended for this problem to be specified vaguely (ie, no given input specs).
+ * You are responsible to gather all the input requirements up front.
+ * <p>
+ * <p>
  * Requirements for atoi:
- *
+ * <p>
  * The function first discards as many whitespace characters as necessary until the first
  * non-whitespace character is found. Then, starting from this character, takes an optional
  * initial plus or minus sign followed by as many numerical digits as possible, and interprets
  * them as a numerical value.
- *
+ * <p>
  * The string can contain additional characters after those that form the integral number,
  * which are ignored and have no effect on the behavior of this function.
- *
+ * <p>
  * If the first sequence of non-whitespace characters in str is not a valid integral number,
  * or if no such sequence exists because either str is empty or it contains only whitespace
  * characters, no conversion is performed.
- *
+ * <p>
  * If no valid conversion could be performed, a zero value is returned. If the correct value
  * is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648)
  * is returned.
- *
  */
 public class StringToInteger {
-    public int atoi(String source) {
-        int result = 0;
+  public int atoi(String source) {
+    int result = 0;
 
-        source=source.trim();
+    source = source.trim();
 
-        if (source == null || source.isEmpty()) {
-            return result;
-        }
-
-        boolean neg = source.charAt(0) == '-' ? true : false;
-        if (neg || source.charAt(0) == '+') {
-            source = source.substring(1, source.length());
-        }
-
-        for (int i = 0; i < source.length(); i++) {
-            int digit = source.charAt(i) - '0';
-            if (neg) {
-                if (-result < (Integer.MIN_VALUE + digit) / 10) {
-                    return Integer.MIN_VALUE;
-                }
-            } else {
-                if (result > (Integer.MAX_VALUE - digit) / 10) {
-                    return Integer.MAX_VALUE;
-                }
-            }
-
-            result = result * 10 + digit;
-        }
-
-        return neg ? -result : result;
+    if (source == null || source.isEmpty()) {
+      return result;
     }
 
-    public static void main(String[] args) {
-        StringToInteger obj = new StringToInteger();
-        System.out.println(obj.atoi("123"));
-        System.out.println(obj.atoi("   123"));
-        System.out.println(obj.atoi("+123"));
-        System.out.println(obj.atoi(" -123"));
-        System.out.println(obj.atoi("123ABC"));
-        System.out.println(obj.atoi(" abc123ABC"));
-        System.out.println(obj.atoi("2147483647"));
-        System.out.println(obj.atoi("-2147483647"));
-        System.out.println(obj.atoi("2147483648"));
-        System.out.println(obj.atoi("-2147483649"));
+    boolean neg = source.charAt(0) == '-' ? true : false;
+    if (neg || source.charAt(0) == '+') {
+      source = source.substring(1, source.length());
     }
+
+    for (int i = 0; i < source.length(); i++) {
+      int digit = source.charAt(i) - '0';
+      if (digit > 9 || digit < 0) {
+        break;
+      }
+
+      if (neg) {
+        if (-result < (Integer.MIN_VALUE + digit) / 10) {
+          return Integer.MIN_VALUE;
+        }
+      } else {
+        if (result > (Integer.MAX_VALUE - digit) / 10) {
+          return Integer.MAX_VALUE;
+        }
+      }
+
+      result = result * 10 + digit;
+    }
+
+    return neg ? -result : result;
+  }
+
+  public static void main(String[] args) {
+    StringToInteger obj = new StringToInteger();
+    System.out.println(obj.atoi("123"));
+    System.out.println(obj.atoi("   123"));
+    System.out.println(obj.atoi("+123"));
+    System.out.println(obj.atoi(" -123"));
+    System.out.println(obj.atoi("123ABC"));
+    System.out.println(obj.atoi(" abc123ABC"));
+    System.out.println(obj.atoi("2147483647"));
+    System.out.println(obj.atoi("-2147483647"));
+    System.out.println(obj.atoi("2147483648"));
+    System.out.println(obj.atoi("-2147483649"));
+  }
 }
--- src/main/java/xiuqin/leetcode/easy/ZigZagConversion.java
+++ src/main/java/xiuqin/leetcode/easy/ZigZagConversion.java
@@ -7,9 +7,9 @@ import java.util.List;
 /**
  * https://leetcode.com/problems/zigzag-conversion/
  * 6. Z 字形变换
- * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行Z 字形排列。
  * <p>
- * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ * 比如输入字符串为 "PAYPALISHIRING"行数为 3 时，排列如下：
  * P   A   H   N
  * A P L S I I G
  * Y   I   R
@@ -19,7 +19,7 @@ import java.util.List;
  * 请你实现这个将字符串进行指定行数变换的函数：
  * <p>
  * string convert(string s, int numRows);
- *  
+ *
  * <p>
  * 示例 1：
  * 输入：s = "PAYPALISHIRING", numRows = 3
@@ -37,7 +37,7 @@ import java.util.List;
  * 示例 3：
  * 输入：s = "A", numRows = 1
  * 输出："A"
- *  
+ *
  * 提示：
  * <p>
  * 1 <= s.length <= 1000
