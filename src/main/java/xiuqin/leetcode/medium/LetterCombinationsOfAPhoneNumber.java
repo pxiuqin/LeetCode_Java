d9133395828387c4f7a33aef8302b8301773752c
liangxiuqin <liangxiuqin@mininglamp.com>
2021-10-15 12:35:26 +0800

    update algo:Letter Combinations Of A Phone Number

--- src/main/java/xiuqin/leetcode/medium/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/xiuqin/leetcode/medium/LetterCombinationsOfAPhoneNumber.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * https://leetcode.com/problems/letter-combinations-of-a-phone-number/
  * 17. 电话号码的字母组合
- * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ * 给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
  * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
  * ./doc/img/17_telephone_keypad.png
  * <p>
@@ -23,7 +23,7 @@ import java.util.List;
  * 示例 3：
  * 输入：digits = "2"
  * 输出：["a","b","c"]
- *  
+ *
  * 提示：
  * 0 <= digits.length <= 4
  * digits[i] 是范围 ['2', '9'] 的一个数字。
--- src/main/java/xiuqin/leetcode/medium/ThreeSumClosest.java
+++ src/main/java/xiuqin/leetcode/medium/ThreeSumClosest.java
@@ -7,19 +7,19 @@ import java.util.List;
 /**
  * https://leetcode.com/problems/3sum-closest/
  * 16. 最接近的三数之和
- * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。
- * 找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。
+ * 给定一个包括n 个整数的数组nums和 一个目标值target。
+ * 找出nums中的三个整数，使得它们的和与target最接近。返回这三个数的和。
  * 假定每组输入只存在唯一答案。
  * <p>
  * 示例：
  * 输入：nums = [-1,2,1,-4], target = 1
  * 输出：2
  * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
- *  
+ *
  * 提示：
  * 3 <= nums.length <= 10^3
- * -10^3 <= nums[i] <= 10^3
- * -10^4 <= target <= 10^4
+ * -10^3<= nums[i]<= 10^3
+ * -10^4<= target<= 10^4
  */
 public class ThreeSumClosest {
     int threeSumClosest(List<Integer> num, int target) {
